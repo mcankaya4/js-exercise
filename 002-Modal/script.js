bf827aca5809a46834e3db65be12aaca9dc5c6f7
mcankaya4 <mcankaya4@gmail.com>
2024-01-05 01:29:10 +0300

    002 js düzenlendi.

--- 002-Modal/script.js
+++ 002-Modal/script.js
@@ -1,25 +1,31 @@
 'use strict';
 
 // Element Selections
+const openBtns = document.querySelectorAll('.show-modal');
 const modalDiv = document.querySelector('.modal');
 const closeBtn = document.querySelector('.close-modal');
 const overlayDiv = document.querySelector('.overlay');
 
 // Modal Opened Event Function
 const openModal = function () {
-  modalDiv.className = 'modal';
-  overlayDiv.className = 'overlay';
+  modalDiv.classList.remove('hidden');
+  overlayDiv.classList.remove('hidden');
   document.addEventListener('keydown', keyHandler);
 };
 
+// Modal Opened Event Listener
+openBtns.forEach((element) => {
+  element.addEventListener('click', openModal);
+});
+
 // Modal Closed Event Function
 const closeEvent = function () {
-  modalDiv.className = 'modal hidden';
-  overlayDiv.className = 'overlay hidden';
+  modalDiv.classList.add('hidden');
+  overlayDiv.classList.add('hidden');
   document.removeEventListener('keydown', keyHandler);
 };
 
-// Modal Closed Event
+// Modal Closed Event Listener
 closeBtn.addEventListener('click', closeEvent);
 overlayDiv.addEventListener('click', closeEvent);
 const keyHandler = function (event) {
